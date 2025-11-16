e745d695582eae99539f0cb94c804025c1bef137
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-11-16 20:45:23 +0530

    Completed

---
+++ Day-06-Smart-Blog-Backend/src/routes/authRoutes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { getMyProfile, login, register, adminRegister, refreshAccessToken } from "../controllers/authController";
+import { authenticate } from "../middleware/auth";
+import { authorization } from "../middleware/authorizeRoles";
+import { Role } from "../models/userModels";
+
+const router = Router()
+
+router.post("/refresh", refreshAccessToken)
+
+router.post("/login", login)
+router.post("/register", register)
+
+// protected - need middleware
+// have access to USER, AUTHOR, ADMIN
+router.get("/me", authenticate, getMyProfile)
+// if admintath access krnna beriwa thiynna onenam
+//router.get("/me", authenticate, authorization("USER"), getMyProfile)
+
+// protected
+// ADMIN only
+// need to create middleware for ensure the req is from ADMIN
+router.post("/admin/register", authenticate, authorization(Role.ADMIN, Role.AUTHOR), adminRegister)
+
+export default router
